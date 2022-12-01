2233ee2ebf351064bedf4e2688989a77590fcd3f
Peter <petroyevenchuk@gmail.com>
2022-12-01 23:08:08 +0200

    FirstPush
    MVC

---
+++ MVC/MVC/DataBasa/DataBase.cs
@@ -0,0 +1,58 @@
+﻿using System.Text;
+
+namespace MVC{
+    static class DataBase{
+
+        public static List<string> dataList = new List<string>();
+
+        public static string GetAnser(string order, string nameData){
+            // create an object (reader)   
+            using (StreamReader sr = new(nameData, Encoding.Default)){
+                string str = null;
+                while (true){
+                    ///if empty, that's the end 
+                    str = sr.ReadLine();
+                    if (str == null){
+                        //if the line is read     
+                        //equal to the search string  
+                        //if (str == order) return "is available.";
+                        break;
+                    }
+                    dataList.Add(str);
+                    //return "not available.";
+                }
+            }
+            string normaliseItem = "";
+            foreach (var item in dataList)
+            {
+                normaliseItem = item.Trim();
+                if(normaliseItem == order)
+                {
+                    return normaliseItem + " is available.";
+                }
+            }
+            return normaliseItem + " not available.";
+        }
+
+        public static void GetData(string nameData){
+            string line;
+            try{
+                //Pass the file path and file name to the StreamReader constructor
+                StreamReader sr = new StreamReader(nameData);
+                //Read the first line of text
+                line = sr.ReadLine();
+                //Continue to read until you reach end of file
+                while (line != null){
+                    //write the line to console window
+                    Console.WriteLine(" - " + line);
+                    //Read the next line
+                    line = sr.ReadLine();
+                }
+                sr.Close();
+            }
+            catch (Exception e){
+                Console.WriteLine("Exception: " + e.Message);
+            }
+        }
+    }
+}
